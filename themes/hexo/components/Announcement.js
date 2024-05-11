061819e7c3d980287713f311156efddc37609698
Weitacha <85831578+Weitacha@users.noreply.github.com>
2024-05-12 04:57:29 +0800

    Update Announcement.js

--- themes/hexo/components/Announcement.js
+++ themes/hexo/components/Announcement.js
@@ -3,19 +3,23 @@ import dynamic from 'next/dynamic'
 
 const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
-const Announcement = ({ post, className }) => {
-  const { locale } = useGlobal()
-  if (post?.blockMap) {
-    return <div className={className}>
-        <section id='announcement-wrapper' className="dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray">
-            <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>
-            {post && (<div id="announcement-content">
-            <NotionPage post={post} className='text-center' />
-        </div>)}
-        </section>
-    </div>
-  } else {
-    return <></>
-  }
-}
+const Announcement = ({ post, className }) => {  
+  const { locale } = useGlobal()  
+  if (post?.blockMap) {  
+    return (  
+      <div className={className}>  
+        <section id='announcement-wrapper' className="dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray">  
+          <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>  
+          {post && (  
+            <div id="announcement-content" className="text-left"> {/* 将 text-center 改为 text-left */}  
+              <NotionPage post={post} /> {/* 移除了传递给 NotionPage 的 className 属性，因为它不再需要 */}  
+            </div>  
+          )}  
+        </section>  
+      </div>  
+    )  
+  } else {  
+    return <></>  
+  }  
+}  
 export default Announcement
